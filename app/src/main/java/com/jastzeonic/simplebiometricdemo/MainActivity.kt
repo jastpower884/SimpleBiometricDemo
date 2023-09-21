64e298e1c8a12b1f2062342877b56fac6002356a
jastlai <jast.lai@datameshgroup.com>
2023-09-21 14:06:24 +1000

    Add the encryption and decryption funtion.

--- app/src/main/java/com/jastzeonic/simplebiometricdemo/MainActivity.kt
+++ app/src/main/java/com/jastzeonic/simplebiometricdemo/MainActivity.kt
@@ -1,22 +1,24 @@
 package com.jastzeonic.simplebiometricdemo
 
-import android.os.Build
 import android.os.Bundle
 import android.security.keystore.KeyGenParameterSpec
 import android.security.keystore.KeyProperties
-import android.util.Log
 import android.widget.Toast
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.appcompat.app.AppCompatActivity
 import androidx.biometric.BiometricManager.Authenticators.BIOMETRIC_STRONG
 import androidx.biometric.BiometricManager.Authenticators.DEVICE_CREDENTIAL
 import androidx.biometric.BiometricPrompt
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.content.ContextCompat
@@ -24,33 +26,27 @@ import androidx.fragment.app.FragmentActivity
 import com.jastzeonic.simplebiometricdemo.ui.theme.SimpleBiometricDemoTheme
 import java.nio.charset.Charset
 import java.security.KeyStore
-import java.util.Arrays
 import java.util.concurrent.Executor
 import javax.crypto.Cipher
 import javax.crypto.KeyGenerator
 import javax.crypto.SecretKey
+import javax.crypto.spec.IvParameterSpec
 
 private const val KEY_NAME = "KEY_NAME"
 
 class MainActivity : FragmentActivity() {
 
     private lateinit var executor: Executor
-    private lateinit var biometricPrompt: BiometricPrompt
-    private lateinit var promptInfo: BiometricPrompt
+    private val contentText: MutableState<ByteArray?> = mutableStateOf("Hello".toByteArray())
+    private var iv: MutableState<ByteArray?> = mutableStateOf(null)
 
+    private lateinit var biometricPrompt: BiometricPrompt
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         executor = ContextCompat.getMainExecutor(this)
-
-        val promptInfo = BiometricPrompt.PromptInfo.Builder()
-            .setTitle("title")
-            .setSubtitle("subtitle")
-            .setAllowedAuthenticators(BIOMETRIC_STRONG or DEVICE_CREDENTIAL)
-            .build()
-
         val activity: FragmentActivity = this
-        val biometricPrompt = BiometricPrompt(activity, executor,
+        biometricPrompt = BiometricPrompt(activity, executor,
             object : BiometricPrompt.AuthenticationCallback() {
                 override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
                     super.onAuthenticationError(errorCode, errString)
@@ -63,10 +59,17 @@ class MainActivity : FragmentActivity() {
 
                 override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
                     super.onAuthenticationSucceeded(result)
-                    Log.i("TagTag","$result")
-                    val encryptedInfo: ByteArray? = result.cryptoObject?.cipher?.doFinal("Hello".toByteArray(Charset.defaultCharset()))
-                    Log.d("MY_APP_TAG", "Encrypted information: " +
-                            Arrays.toString(encryptedInfo))
+
+                    if (iv.value == null) {
+                        val encryptedInfo = result.cryptoObject?.cipher?.doFinal(String(contentText.value ?: byteArrayOf()).toByteArray(Charset.defaultCharset()))
+                        contentText.value = encryptedInfo
+                        iv.value = result.cryptoObject?.cipher?.iv
+                    } else {
+                        val decryptedInfo: ByteArray? = result.cryptoObject?.cipher?.doFinal(contentText.value)
+                        contentText.value = decryptedInfo
+                        iv.value = null
+                    }
+
                     Toast.makeText(activity.applicationContext, "succeeded!", Toast.LENGTH_LONG).show()
                 }
 
@@ -79,40 +82,126 @@ class MainActivity : FragmentActivity() {
             }
         )
 
-        generateSecretKey(KeyGenParameterSpec.Builder(
-            KEY_NAME,
-            KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT)
-            .setBlockModes(KeyProperties.BLOCK_MODE_CBC)
-            .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_PKCS7)
-            .setUserAuthenticationRequired(true)
-            // Invalidate the keys if the user has registered a new biometric
-            // credential, such as a new fingerprint. Can call this method only
-            // on Android 7.0 (API level 24) or higher. The variable
-            // "invalidatedByBiometricEnrollment" is true by default.
-            .setInvalidatedByBiometricEnrollment(true)
-            .build())
-
-
-        val cipher = getCipher()
-        val secretKey = getSecretKey()
-        cipher.init(Cipher.ENCRYPT_MODE, secretKey)
-        biometricPrompt.authenticate(promptInfo,BiometricPrompt.CryptoObject(cipher))
-        BiometricPrompt.CryptoObject(cipher)
 
         setContent {
             SimpleBiometricDemoTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    ButtonZone()
                 }
             }
         }
     }
 
+    @Composable
+    private fun ButtonZone() {
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+
+                Text(text = String(contentText.value ?: byteArrayOf()))
+
+                Button(
+                    enabled = (iv.value?.isEmpty() ?: true),
+                    onClick = { encryption() }) {
+                    Text("Encryption")
+
+                }
+                Button(
+                    enabled = iv.value?.isNotEmpty() == true,
+                    onClick = { decryption() }) {
+                    Text("Decryption")
+
+                }
+            }
+
+        }
+    }
+
+    private fun encryption() {
+
+        val promptInfo = BiometricPrompt.PromptInfo.Builder()
+            .setTitle("title")
+            .setSubtitle("subtitle")
+            .setAllowedAuthenticators(BIOMETRIC_STRONG or DEVICE_CREDENTIAL)
+            .build()
+
+
+        val cipher = getCipher()
+        val secretKey = try {
+            getSecretKey()
+        } catch (ex: Exception) {
+            generateSecretKey(
+                KeyGenParameterSpec.Builder(KEY_NAME, KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT)
+                    .setBlockModes(KeyProperties.BLOCK_MODE_CBC)
+                    .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_PKCS7)
+                    .setUserAuthenticationRequired(true)
+                    // Invalidate the keys if the user has registered a new biometric
+                    // credential, such as a new fingerprint. Can call this method only
+                    // on Android 7.0 (API level 24) or higher. The variable
+                    // "invalidatedByBiometricEnrollment" is true by default.
+                    .setInvalidatedByBiometricEnrollment(true)
+                    .build()
+            )
+
+            getSecretKey()
+        }
+
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey)
+        biometricPrompt.authenticate(promptInfo, BiometricPrompt.CryptoObject(cipher))
+        BiometricPrompt.CryptoObject(cipher)
+    }
+
+    private fun decryption() {
+
+        val promptInfo = BiometricPrompt.PromptInfo.Builder()
+            .setTitle("title")
+            .setSubtitle("subtitle")
+            .setAllowedAuthenticators(BIOMETRIC_STRONG or DEVICE_CREDENTIAL)
+            .build()
+
+        val cipher = getCipher()
+        val secretKey = try {
+            getSecretKey()
+        } catch (ex: Exception) {
+            generateSecretKey(
+                KeyGenParameterSpec.Builder(KEY_NAME, KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT)
+                    .setBlockModes(KeyProperties.BLOCK_MODE_CBC)
+                    .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_PKCS7)
+                    .setUserAuthenticationRequired(true)
+                    // Invalidate the keys if the user has registered a new biometric
+                    // credential, such as a new fingerprint. Can call this method only
+                    // on Android 7.0 (API level 24) or higher. The variable
+                    // "invalidatedByBiometricEnrollment" is true by default.
+                    .setInvalidatedByBiometricEnrollment(true)
+                    .build()
+            )
+
+            getSecretKey()
+        }
+
+        cipher.init(Cipher.DECRYPT_MODE, secretKey, IvParameterSpec(iv.value))
+        biometricPrompt.authenticate(promptInfo, BiometricPrompt.CryptoObject(cipher))
+        BiometricPrompt.CryptoObject(cipher)
+    }
+
+    private fun getCipher(): Cipher {
+        return Cipher.getInstance(
+            KeyProperties.KEY_ALGORITHM_AES + "/"
+                    + KeyProperties.BLOCK_MODE_CBC + "/"
+                    + KeyProperties.ENCRYPTION_PADDING_PKCS7
+        )
+    }
 
     private fun generateSecretKey(keyGenParameterSpec: KeyGenParameterSpec) {
+
         val keyGenerator = KeyGenerator.getInstance(
-            KeyProperties.KEY_ALGORITHM_AES, "AndroidKeyStore")
+            KeyProperties.KEY_ALGORITHM_AES, "AndroidKeyStore"
+        )
         keyGenerator.init(keyGenParameterSpec)
         keyGenerator.generateKey()
     }
@@ -125,26 +214,12 @@ class MainActivity : FragmentActivity() {
         return keyStore.getKey(KEY_NAME, null) as SecretKey
     }
 
-    private fun getCipher(): Cipher {
-        return Cipher.getInstance(KeyProperties.KEY_ALGORITHM_AES + "/"
-                + KeyProperties.BLOCK_MODE_CBC + "/"
-                + KeyProperties.ENCRYPTION_PADDING_PKCS7)
-    }
 
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     SimpleBiometricDemoTheme {
-        Greeting("Android")
     }
 }
